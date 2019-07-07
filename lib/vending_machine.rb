56a19d4b311c654042bef9471ff2e22b7a78a2fa
Jordan Roberts <jordanroberts@talk21.com>
2019-07-07 20:46:25 +0100

    Basic purchase method implemented

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,13 +2,22 @@ class Vending_Machine
 
   attr_reader :change, :items
 
-  def initialize
-    @change = {}
-    @items = {}
+  def initialize(items = [], change = 0)
+    @items = items
+    @change = change
+  end
+  
+  def restock
+    @items = [
+            {:name=>"Cheetos", :quantity=>10, :price=>0.50},
+            {:name=>"Pringles", :quantity=>5, :price=>0.60}
+            ]
   end
 
-  def restock(price, item)
-    @items = { 0.5 => "pringles" }
+  def purchase(item, coins)
+    item = @items.find {|product| product[:name] == item }
+    item[:quantity] -= 1
+    @change += item[:price]
+    return @items
   end
-
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -6,9 +6,16 @@ describe Vending_Machine do
 
   describe '#restock' do
     it 'Items can be added to vending machine' do
-      expect(machine.restock(0.5, "pringles")).to eq({ 0.5 => "pringles" })
+      expect(machine.restock).to include({:name=>"Cheetos", :price=>0.5, :quantity=>10})
     end
   end
 
+  describe '#purchase' do
+    it 'Items can be purchased from vending machine' do
+      machine.restock
+      machine.purchase("Cheetos", 0.5)
+      expect(machine.restock).to include({:name=>"Cheetos", :price=>0.5, :quantity=>10})
+    end
+  end
 
 end
